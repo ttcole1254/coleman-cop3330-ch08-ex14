2cead4549c09e95f84adcb9d6413e9f741736b92
Test <Test@test.com>
2021-11-27 22:29:41 -0500

    Init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Tyler Coleman
+ */
+
+/*
+* Can we declare a non-reference function argument const (e.g., void f(const int);)? What might that mean? Why might
+* we want to do that? Why don’t people do that often? Try it; write a couple of small programs to see what works.
+*/
